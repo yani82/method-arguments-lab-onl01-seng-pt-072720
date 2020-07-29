0e97a5532f2d0fbcce99d27cbb8b04e7e2beface
Yani Asari <yani.iman1@gmail.com>
2020-07-29 02:30:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,7 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-puts name + ',' + language 
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
 
