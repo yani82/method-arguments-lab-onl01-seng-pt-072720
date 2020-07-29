063888b7e4ce68e58b6d67bb313c12a38de935f2
Yani Asari <yani.iman1@gmail.com>
2020-07-29 02:31:01 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,13 @@
+
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+  # puts "Hi, my name is " + name.to_s + "."
+  # name will serve as a local variable inside of our method body
+end
+
+def introduction_with_language(name, language)
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
+
+
